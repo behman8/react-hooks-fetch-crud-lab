d335f35771e22c79c1103d50eb8ddec6a131c5c1
Branden <brandenehman45@gmail.com>
2021-08-02 09:43:11 -0600

    Trying to display all the questions

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function QuestionList() {
+  const [questionItem, setQuestionItem] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((resp) => resp.json())
+      .then((data) => {
+        data.map((question) => setQuestionItem(question.prompt))
+      });
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItem}</ul>
     </section>
   );
 }
